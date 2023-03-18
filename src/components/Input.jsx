852166048a67c2fc308b5dadbe92b9a4e9f81286
Yusuf <ysfayds41@gmail.com>
2023-03-18 20:36:27 +0300

    edit

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,5 @@
 import { TextField } from "@mui/material"
+import { hover } from "@testing-library/user-event/dist/hover"
 
 const Input = ({output,setOutput,input,setInput}) => {
   return (
@@ -8,7 +9,7 @@ const Input = ({output,setOutput,input,setInput}) => {
           multiline
           rows={20}
           
-        sx={{width:"50%",boxShadow: "rgba(0, 0, 0, 0.15) 6px 6px 3px"}}
+        sx={{width:"50%",boxShadow: "rgba(0, 0, 0, 0.15) 6px 6px 3px","&:hover":{border:"0",outline:"0"}}}
           defaultValue={input}
           onChange={(e)=> setInput(e.target.value) }
         />
